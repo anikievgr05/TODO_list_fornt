99b191792febf35123c8c9e1bf7bc28bb0983bb3
anikievgr <anikievgr.2005@gmail.com>
2025-04-27 22:45:22 +0300

    страница проекта

--- next_front/src/app/(authenticated)/[project_name]/page.tsx
+++ next_front/src/app/(authenticated)/[project_name]/page.tsx
@@ -1,20 +1,23 @@
 'use client'
 
 import {useAuth} from '@/hooks/auth'
-import LoginLinks from "@/app/LoginLinks";
-import Profile from "@/components/Icons/Profile";
-import CreateTask from "@/components/Icons/CreateTask";
-import Wiki from "@/components/Icons/Wiki";
-import Settings from "@/components/Icons/Settings";
-import MainTasks from "@/components/Icons/MainTasks";
-import IconLink from "@/components/IconLink";
+import React from "react";
+import {useProjectContext} from "@/app/context/ProjectContext";
 
 
 export default function Home() {
     const {user} = useAuth({middleware: 'guest'})
+    const { projectContext, setProjectContext } = useProjectContext();
+
     return (
-        <>
-            sdfg
-        </>
+        <div className="py-12">
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="p-6 bg-white border-b border-gray-200 text-dark_charcoal">
+                        Вы на проекте: {projectContext?.name}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
